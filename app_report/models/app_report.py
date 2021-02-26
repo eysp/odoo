831270cf950ababbb77780255e2bf37c33e404aa
Abhay shukla <33155803+rudra-shukla@users.noreply.github.com>
2021-02-26 17:41:43 +0530

    Update app_report.py

--- app_report/models/app_report.py
+++ app_report/models/app_report.py
@@ -10,7 +10,7 @@ class SaleOrder_Data(models.Model):
     description_1 = fields.Char(string = 'Description 1')
     dealer_discount = fields.Char(string = 'Dealer Discount1')
     check_it = fields.Boolean(string = 'check it', help= 'this is just to test booleean field')
-    result = fields.Float(string='result' digits=(12,3))
+    result = fields.Float(string='result' digits=(12,4))
     
     
 
