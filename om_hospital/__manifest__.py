1ee22de390a4d3fa76a977a953e171fc8bc580a5
Abhay shukla <33155803+rudra-shukla@users.noreply.github.com>
2021-03-09 17:02:19 +0530

    Create __manifest__.py

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name' : 'Hospital Management',
+    'version': '1.0',
+    'Summary': 'my first module',
+    'description': 'To mange hospitals',
+    'license': 'LGPL-3',
+    'depends': [],    
+    'data': [],
+    'installable': True,
+    'application':True,
+    'auto_install':False
+}
