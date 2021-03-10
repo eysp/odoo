a77314449ee7105351a4955f4bbbd04ba3bca9a3
Abhay shukla <33155803+rudra-shukla@users.noreply.github.com>
2021-03-10 12:02:18 +0530

    Update patient.py

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -6,4 +6,5 @@ class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Patient records'
 
-    patient_name = feilds.Char(string='Patient Name')
+    patient_name = feilds.Char(string='Name')
+    patient_age = feilds.Integer('age')
