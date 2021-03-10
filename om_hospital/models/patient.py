2bbeef8dcaf5e701d1358a5bc8afee08f137821f
Abhay shukla <33155803+rudra-shukla@users.noreply.github.com>
2021-03-10 12:08:27 +0530

    Update patient.py

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -6,5 +6,5 @@ class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Patient records'
 
-    patient_name = fields.Char(string='Name')
+    patient_name = fields.Char(string='Name' required='True')
     patient_age = fields.Integer('age')
