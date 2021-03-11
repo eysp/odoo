a48b85d4b3f89a285d149379a965a38aeb16a1bd
Abhay shukla <33155803+rudra-shukla@users.noreply.github.com>
2021-03-11 14:35:10 +0530

    for custom

---
+++ custom/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name' : 'Custom',
+    'version': '1.0',
+    'Summary': 'custom ',
+    'description': 'for vikrma sir',
+    'license': 'LGPL-3',
+    'depends': [],    
+    'data': [
+        #'views/patient.xml'
+    ],
+    'installable': True,
+    'application':True,
+    'auto_install':False
+}
