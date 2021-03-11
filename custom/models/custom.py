5f89bb4956ac806d5b14e243a47ab6dc6e2be127
Abhay shukla <33155803+rudra-shukla@users.noreply.github.com>
2021-03-11 15:56:07 +0530

    saleOrder

--- custom/models/custom.py
+++ custom/models/custom.py
@@ -2,6 +2,17 @@ from odoo import models, fields, api
 
 
 
+class SaleOrder_Data(models.Model):
+    _inherit = 'sale.order'
+
+    channel_order_number = fields.Char(string = 'Channel Order')
+    payment_type = fields.Char(string = 'Payment Type')
+    description_1 = fields.Char(string = 'Description 1')
+    unique_id = fields.Char(string = 'Unique Id')
+    check_it = fields.Boolean(string = 'check it', help= 'this is just to test booleean field')
+    result = fields.Float(string='result', digits=(12,6))
+    
+
 class HospitalPatient(models.Model):
     _name = 'custom.module'
     _description = 'created for custom module'
