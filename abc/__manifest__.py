68fb4589efbe3cd4a5ff84282ba2a21deb197e18
rani700 <shwetarani700@gmail.com>
2021-03-11 15:20:14 +0530

    Create __manifest__.py

---
+++ abc/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'abc reports',
+    'version': '1.2',
+    'Summary': 'abc Report Prints',
+    'description': 'To print the new report',
+    'license': 'LGPL-3',
+    'depends': [],    
+    'data': [
+        #'data/data.xml',
+        #'reports/report_tax_invoice.xml',
+        #'reports/reports.xml',
+        #'views/app_report_testview.xml'
+    ],
+    'installable': True,
+    'application':True,
+    'auto_install':False
+}
