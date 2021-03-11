dae153fd3ce0208b6b27b6a3e5afa8ee2deb9060
rani700 <shwetarani700@gmail.com>
2021-03-11 18:17:02 +0530

    updated

---
+++ Custom/models/custom_test.py
@@ -0,0 +1,22 @@
+from odoo import models, fields, api
+
+
+class SaleOrder_Data(models.Model):
+    _inherit = 'sale.order'
+    #description = fields.Char(string='Description')
+
+    channel_order_number = fields.Char(string = 'Channel Order No.')
+    payment_amount = fields.Char(string = 'Payment Amount')
+    Age = fields.Float('Age', digits=(12,4))
+    custom_payment_method = fields.Char(string = 'Custom Payment Method')
+
+
+
+
+ class Custom_Data(models.Model):
+    _name = 'custom.module'
+    _description = 'new custom module'
+
+
+    name = fields.Char(string = 'Name')
+    age = fields.Char(string = 'Name') 
