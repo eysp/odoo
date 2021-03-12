cb001457710269b5e474de0deafad5227962bd3a
Abhay shukla <33155803+rudra-shukla@users.noreply.github.com>
2021-03-12 11:53:58 +0530

    change3

--- custom/models/custom.py
+++ custom/models/custom.py
@@ -4,13 +4,6 @@ from odoo import models, fields, api
 
 class SaleOrder_Data(models.Model):
     _inherit = 'sale.order'
-
-    channel_order_number = fields.Char(string = 'Channel Order')
-    payment_type = fields.Char(string = 'Payment Type')
-    description_1 = fields.Char(string = 'Description 1')
-    unique_id = fields.Char(string = 'Unique Id')
-    check_it = fields.Boolean(string = 'check it', help= 'this is just to test booleean field')
-    result = fields.Float(string='result', digits=(12,6))
     custom_payment_method  = fields.Char(string='P M')
     
 
