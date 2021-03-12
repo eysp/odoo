a41ac36f702eb65e1c17067839716f4462e360ce
Abhay shukla <33155803+rudra-shukla@users.noreply.github.com>
2021-03-12 12:07:41 +0530

    change4

--- custom/models/custom.py
+++ custom/models/custom.py
@@ -5,6 +5,8 @@ from odoo import models, fields, api
 class SaleOrder_Data(models.Model):
     _inherit = 'sale.order'
     custom_payment_method  = fields.Char(string='P M')
+
+    
     
 
 class HospitalPatient(models.Model):
