80fac2919a91ba02b328a1b42776884d04445a50
Abhay shukla <33155803+rudra-shukla@users.noreply.github.com>
2021-03-13 10:52:53 +0530

    Update custom.py

--- custom/models/custom.py
+++ custom/models/custom.py
@@ -5,6 +5,10 @@ from odoo import models, fields, api
 class SaleOrder_Data(models.Model):
     _inherit = 'sale.order'
     custom_payment_method  = fields.Char(string='P M')
+
+class SaleOrder_Data(models.Model):
+    _inherit = 'sale.report'
+    custom_payment_method  = fields.Char(string='P M')
     
     
 #self.env.cr.execute("SELECT custom_payment_method FROM sale.order LEFT JOIN sale.order ON sale.order.Id = sale.report.Id")
