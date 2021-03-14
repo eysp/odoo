15c00de66b0e20dcf46e430237e1604e55069fe4
Abhay shukla <33155803+rudra-shukla@users.noreply.github.com>
2021-03-14 13:09:25 +0530

    Update custom_report.py

--- custom/report/custom_report.py
+++ custom/report/custom_report.py
@@ -8,38 +8,49 @@ class SaleReport(models.Model):
 
     partner_id1 = fields.Many2one('res.partner', 'Customer1', readonly=True)
 
-    def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
-        with_ = ("WITH %s" % with_clause) if with_clause else ""
+    # def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
+    #     with_ = ("WITH %s" % with_clause) if with_clause else ""
 
-        select_ = """
+    #     select_ = """
             
-            s.partner_id1 as partner_id1
+    #         s.partner_id1 as partner_id1
             
-        """
+    #     """
 
-        # for field in fields.values():
-        #     select_ += field
+    #     for field in fields.values():
+    #         select_ += field
 
-        from_ = """
-                sale_order_line l
-                      right outer join sale_order s on (s.id=l.order_id)
-                      join res_partner partner on s.partner_id = partner.id
-                        left join product_product p on (l.product_id=p.id)
-                            left join product_template t on (p.product_tmpl_id=t.id)
-                    left join uom_uom u on (u.id=l.product_uom)
-                    left join uom_uom u2 on (u2.id=t.uom_id)
-                    left join product_pricelist pp on (s.pricelist_id = pp.id)
-                %s
-        """ % from_clause
+    #     from_ = """
+    #             sale_order_line l
+    #                   right outer join sale_order s on (s.id=l.order_id)
+    #                   join res_partner partner on s.partner_id = partner.id
+    #                     left join product_product p on (l.product_id=p.id)
+    #                         left join product_template t on (p.product_tmpl_id=t.id)
+    #                 left join uom_uom u on (u.id=l.product_uom)
+    #                 left join uom_uom u2 on (u2.id=t.uom_id)
+    #                 left join product_pricelist pp on (s.pricelist_id = pp.id)
+    #             %s
+    #     """ % from_clause
 
-        groupby_ = """
-            s.partner_id1
-            %s
-        """ % (groupby)
+    #     groupby_ = """
+    #         s.partner_id1
+    #         %s
+    #     """ % (groupby)
 
-        return '%s (SELECT %s FROM %s GROUP BY %s)' % (with_, select_, from_, groupby_)
+    #     return '%s (SELECT %s FROM %s GROUP BY %s)' % (with_, select_, from_, groupby_)
     
+    # def init(self):
+    #     # self._table = sale_report
+    #     tools.drop_view_if_exists(self.env.cr, self._table)
+    #     self.env.cr.execute("""CREATE or REPLACE VIEW %s as (%s)""" % (self._table, self._query()))
+
     def init(self):
-        # self._table = sale_report
         tools.drop_view_if_exists(self.env.cr, self._table)
-        self.env.cr.execute("""CREATE or REPLACE VIEW %s as (%s)""" % (self._table, self._query()))
+        self.env.cr.execute(
+            """
+                CREATE OR REPLACE VIEW sale.report.search AS (
+                SELECT s.partner_id1 as partner_id1
+                FROM sale.order,sale.report 
+                WHERE s.partner_id1 = partner.id1
+                GROUP BY partner_id1)
+            """ )
