9ff54116fa8c0d7c493eb26408511fee71654290
Abhay shukla <33155803+rudra-shukla@users.noreply.github.com>
2021-03-15 13:16:15 +0530

    mon4

--- custom/models/custom.py
+++ custom/models/custom.py
@@ -10,57 +10,57 @@ from odoo import tools
 class SaleOrder_Data(models.Model):
     _inherit = 'sale.order'
     custom_payment_method  = fields.Char(string='P M')
-    partner_id1 = fields.Many2one(
-        'res.partner', string='Customer1', readonly=True,
-        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
-        required=True, change_default=True, index=True, tracking=1,
-        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",)
+    # partner_id1 = fields.Many2one(
+    #     'res.partner', string='Customer1', readonly=True,
+    #     states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
+    #     required=True, change_default=True, index=True, tracking=1,
+    #     domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",)
 
 
-class SaleReport(models.Model):
-    _inherit = 'sale.report'
-    _description = "Sales Analysis Report"
+# class SaleReport(models.Model):
+#     _inherit = 'sale.report'
+#     _description = "Sales Analysis Report"
     
 
 
     
-    partner_id1 = fields.Many2one('res.partner', 'Customer1', readonly=True)
+#     partner_id1 = fields.Many2one('res.partner', 'Customer1', readonly=True)
 
 
-    def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
-        with_ = ("WITH %s" % with_clause) if with_clause else ""
+#     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
+#         with_ = ("WITH %s" % with_clause) if with_clause else ""
 
-        select_ = """
-            s.partner_id1 as partner_id1
+#         select_ = """
+#             s.partner_id1 as partner_id1
             
-        """
+#         """
 
-        for field in fields.values():
-            select_ += field
+#         for field in fields.values():
+#             select_ += field
 
-        from_ = """
-                sale_order_line l
-                      right outer join sale_order s on (s.id=l.order_id)
-                      join res_partner partner on s.partner_id = partner.id
-                        left join product_product p on (l.product_id=p.id)
-                            left join product_template t on (p.product_tmpl_id=t.id)
-                    left join uom_uom u on (u.id=l.product_uom)
-                    left join uom_uom u2 on (u2.id=t.uom_id)
-                    left join product_pricelist pp on (s.pricelist_id = pp.id)
-                %s
-        """ % from_clause
+#         from_ = """
+#                 sale_order_line l
+#                       right outer join sale_order s on (s.id=l.order_id)
+#                       join res_partner partner on s.partner_id = partner.id
+#                         left join product_product p on (l.product_id=p.id)
+#                             left join product_template t on (p.product_tmpl_id=t.id)
+#                     left join uom_uom u on (u.id=l.product_uom)
+#                     left join uom_uom u2 on (u2.id=t.uom_id)
+#                     left join product_pricelist pp on (s.pricelist_id = pp.id)
+#                 %s
+#         """ % from_clause
 
-        groupby_ = """
-            s.partner_id1
-             %s
-        """ % (groupby)
+#         groupby_ = """
+#             s.partner_id1
+#              %s
+#         """ % (groupby)
 
-        return '%s (SELECT %s FROM %s GROUP BY %s)' % (with_, select_, from_, groupby_)
+#         return '%s (SELECT %s FROM %s GROUP BY %s)' % (with_, select_, from_, groupby_)
 
-    def init(self):
-        # self._table = sale_report
-        tools.drop_view_if_exists(self.env.cr, self._table)
-        self.env.cr.execute("""CREATE or REPLACE VIEW %s as (%s)""" % (self._table, self._query()))
+#     def init(self):
+#         # self._table = sale_report
+#         tools.drop_view_if_exists(self.env.cr, self._table)
+#         self.env.cr.execute("""CREATE or REPLACE VIEW %s as (%s)""" % (self._table, self._query()))
 
     
     
